b4c53a95d91dddcf05d4ddade57d2302efd1c9b9
ozinkevych <zinkevicsasa1@gmail.com>
2024-05-02 13:21:27 +0300

    added a slider to the layout

--- js/script.js
+++ js/script.js
@@ -4,3 +4,31 @@ const mainMenu = document.querySelector('.main-menu');
 burgerMenu.addEventListener('click', () => {
     mainMenu.classList.toggle('show');
 });
+
+$(document).ready(function() {
+    $('.slider').slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        arrows: false,
+        centerMode: true,
+        centerPadding: '0',
+        fade: true,
+        cssEase: 'linear',
+        draggable: false,
+        swipe: false,
+        swipeToSlide: true,
+    });
+
+    $('.prev-slide').click(function() {
+        $('.slider').slick('slickPrev');
+    });
+
+    $('.next-slide').click(function() {
+        $('.slider').slick('slickNext');
+    });
+
+    $('.prev-slide, .next-slide').click(function() {
+        $('.slider').slick('slickPlay');
+    });
+});
+
