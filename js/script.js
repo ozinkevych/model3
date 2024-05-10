79e158af4a9c2f7e5ac4ca2d9d156603530fee0f
ozinkevych <zinkevicsasa1@gmail.com>
2024-05-10 11:56:00 +0300

    made a popup form continue work on validation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
+document.getElementById('openModalBtn').addEventListener('click', function(event) {
+    event.preventDefault(); // Заборона перехіду за посиланням (або виконання події за замовчуванням)
 
+    // Показати модальне вікно
+    document.getElementById('myModal').style.display = 'flex';
+});
+
+// Додати обробник події для закриття модального вікна
+document.querySelector('.close').addEventListener('click', function() {
+    // Закрити модальне вікно
+    document.getElementById('myModal').style.display = 'none';
+});
 
 $(document).ready(function() {
     $('.slider').slick({
@@ -33,3 +44,4 @@ const mainMenu = document.querySelector('.main-menu');
 burgerMenu.addEventListener('click', () => {
     mainMenu.classList.toggle('show');
 });
+
