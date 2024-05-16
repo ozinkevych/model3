63e5b6da4fa68d2df0ae69b97120ad235b8745d3
ozinkevych <zinkevicsasa1@gmail.com>
2024-05-16 12:06:24 +0300

    fixed an error in the form and added a phone link

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
-document.getElementById('open-Modal-Btn').addEventListener('click', function(event) {
-    event.preventDefault(); // Prohibition of clicking on a link (or executing an event by default)
+document.getElementById('open-Modal-Btn').addEventListener('click', function (event) {
+    event.preventDefault();
 
-    // Show modal window
     document.getElementById('myModal').style.display = 'flex';
 });
 
-// Add an event handler for closing the modal window
-document.querySelector('.close').addEventListener('click', function() {
-    // Close the modal window
+
+document.querySelector('.close').addEventListener('click', function () {
     document.getElementById('myModal').style.display = 'none';
 });
 
-$(document).ready(function() {
+
+$(document).ready(function () {
     $('.slider').slick({
         autoplay: true,
         autoplaySpeed: 4000,
@@ -24,19 +23,21 @@ $(document).ready(function() {
         swipe: false,
         swipeToSlide: true,
     });
-
-    $('.prev-slide').click(function() {
+    $('.prev-slide, .next-slide').click(function () {
+        $('.slider').slick('slickPlay');
+    });
+    $('.prev-slide').click(function () {
         $('.slider').slick('slickPrev');
     });
 
-    $('.next-slide').click(function() {
+    $('.next-slide').click(function () {
         $('.slider').slick('slickNext');
     });
 
-    $('.prev-slide, .next-slide').click(function() {
-        $('.slider').slick('slickPlay');
-    });
+
 });
+
+
 
 const burgerMenu = document.querySelector('.burger-menu');
 const mainMenu = document.querySelector('.main-menu');
@@ -44,4 +45,13 @@ const mainMenu = document.querySelector('.main-menu');
 burgerMenu.addEventListener('click', () => {
     mainMenu.classList.toggle('show');
 });
+document.querySelector('.form-btn').addEventListener('click', function (event) {
+    let form = document.querySelector('.feedback-form');
 
+    if (!form.checkValidity()) {
+        event.preventDefault();
+        form.classList.add('invalid');
+    } else {
+        form.classList.remove('invalid');
+    }
+});
